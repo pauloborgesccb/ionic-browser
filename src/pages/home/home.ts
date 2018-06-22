ea410a41b9db9ab22d1282e496d75b8820d6d6c3
pauloborges <contrictorhkss@gamil.com>
2018-06-22 00:11:34 -0300

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {InAppBrowser} from "@ionic-native/in-app-browser";
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  host: string = 'localhost';
+  port: string= '8100';
+
+  constructor(private iab: InAppBrowser) {}
+
+  openUrl() {
+      if (!this.port || this.port) {
+          alert('Informe host e porta');
+      }
+      const browser = this.iab.create(
+          'http://' + this.host + ':' + this.port,
+          '_self',
+          {location: 'no',toolbar: 'no'}
+      );
+      browser.show();
+  }
+
+}
